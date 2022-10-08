ffa04aa853695e21baf17e3bc807580c42c1a425
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-10-08 11:54:03 +0300

    add_start_components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import user from 'components/user.json';
+import data from 'components/data.json';
+import friends from 'components/friends.json';
+import transactions from 'components/transactions.json';
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
